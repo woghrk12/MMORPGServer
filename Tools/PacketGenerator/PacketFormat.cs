c17fa6d7c91356f49f92671b3edcb8d591bbc627
황재호 <>
2024-07-07 21:30:02 +0900

    Modify the packet format string to be immutable

--- Tools/PacketGenerator/PacketFormat.cs
+++ Tools/PacketGenerator/PacketFormat.cs
@@ -6,7 +6,7 @@ namespace PacketGenerator
         /// {0} : The enum elements representing the index of the packet. <br/>
         /// {1} : The classes representing the packet. <br/>
         /// </summary>
-        public static string FILE_FORMAT =
+        public static readonly string FILE_FORMAT =
 @"using System;
 using System.Collections.Generic;
 using System.Text;
@@ -25,7 +25,7 @@ public enum EPacketID
         /// {0} : The packet name with all letters in uppercase and spaces replaced by underscores. <br/>
         /// {1} : The index of the packet. <br/>
         /// </summary>
-        public static string PACKET_ENUM_FORMAT =
+        public static readonly string PACKET_ENUM_FORMAT =
 @"{0} = {1},";
 
         /// <summary>
@@ -35,7 +35,7 @@ public enum EPacketID
         /// {3} : The logic to read the member variables from the packet. <br/>
         /// {4} : The logic to write the member variables to the packet. <br/>
         /// </summary>
-        public static string PACKET_FORMAT =
+        public static readonly string PACKET_FORMAT =
 @"public class {0}
 {{
     #region Variables
@@ -86,7 +86,7 @@ public enum EPacketID
         /// {0} : The type of the member variable. <br/>
         /// {1} : The name of the member variable. <br/>
         /// </summary>
-        public static string MEMBER_FORMAT =
+        public static readonly string MEMBER_FORMAT =
 @"public {0} {1};";
 
         /// <summary>
@@ -96,7 +96,7 @@ public enum EPacketID
         /// {3} : The logic to read the member variables from the packet. <br/>
         /// {4} : The logic to write the member variables to the packet. <br/>
         /// </summary>
-        public static string MEMBER_LIST_FORMAT =
+        public static readonly string MEMBER_LIST_FORMAT =
 @"
 public class {0}
 {{
@@ -124,7 +124,7 @@ public List<{0}> {1}List {{ private set; get; }} = new();";
         /// {1} : The name of the method to convert a byte array to the type of the member variable. <br/>
         /// {2} : The type of the member variable to read from the packet. <br/>
         /// </summary>
-        public static string READ_FORMAT =
+        public static readonly string READ_FORMAT =
 @"this.{0} = BitConverter.{1}(span.Slice(count, span.Length - count));
 count += sizeof({2});";
 
@@ -132,14 +132,14 @@ count += sizeof({2});";
         /// {0} : The name of the member variable to read from the packet. <br/>
         /// {1} : The type of the member variable to read from the packet. <br/>
         /// </summary>
-        public static string READ_BYTE_FORMAT =
+        public static readonly string READ_BYTE_FORMAT =
 @"this.{0} = ({1})segment.Array[segment.Offset + count];
 count += sizeof({1}});";
 
         /// <summary>
         /// {0} : The name of the member variable of type string to read from the packet. <br/>
         /// </summary>
-        public static string READ_STRING_FORMAT =
+        public static readonly string READ_STRING_FORMAT =
 @"ushort {0}Len = BitConverter.ToUInt16(span.Slice(count, span.Length - count));
 count += sizeof(ushort);
 this.{0} = Encoding.Unicode.GetString(span.Slice(count, {0}Len));
@@ -150,7 +150,7 @@ count += {0}Len;";
         /// {0} : The name of the struct, starting with a capital letter, to be used as the element type of the list. <br/>
         /// {1} : The name of the struct, starting with a lowercase letter, to be used as the element type of the list. <br/>
         /// </summary>
-        public static string READ_LIST_FORMAT =
+        public static readonly string READ_LIST_FORMAT =
 @"this.{1}List.Clear();
 ushort {1}Len = BitConverter.ToUInt16(span.Slice(count, span.Length - count));
 count += sizeof(ushort);
@@ -165,7 +165,7 @@ for (int index = 0; index < {1}Len; index++)
         /// {0} : The name of the member variable to write to the packet. <br/>
         /// {1} : The type of the member variable to write to the packet. <br/>
         /// </summary>
-        public static string WRITE_FORMAT =
+        public static readonly string WRITE_FORMAT =
 @"isSuccess &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), this.{0});
 count += sizeof({1});";
 
@@ -173,14 +173,14 @@ count += sizeof({1});";
         /// {0} : The name of the member variable to write to the packet. <br/>
         /// {1} : The type of the member variable to write to the packet. <br/>
         /// </summary>
-        public static string WRITE_BYTE_FORMAT =
+        public static readonly string WRITE_BYTE_FORMAT =
 @"segment.Array[segment.Offset + count] = (byte)this.{0};
 count += sizeof({1}});";
 
         /// <summary>
         /// {0} : The name of the member variable of type string to write to the packet. <br/>
         /// </summary>
-        public static string WRITE_STRING_FORMAT =
+        public static readonly string WRITE_STRING_FORMAT =
 @"ushort {0}Len = (ushort)Encoding.Unicode.GetBytes(this.{0}, 0, this.{0}.Length, segment.Array, segment.Offset + count + sizeof(ushort));
 isSuccess &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), {0}Len);
 count += sizeof(ushort);
@@ -190,7 +190,7 @@ count += {0}Len;";
         /// {0} : The name of the struct, starting with a capital letter, to be used as the element type of the list. <br/>
         /// {1} : The name of the struct, starting with a lowercase letter, to be used as the element type of the list. <br/>
         /// </summary>
-        public static string WRITE_LIST_FORMAT =
+        public static readonly string WRITE_LIST_FORMAT =
 @"isSuccess &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), (ushort)this.{1}List.Count);
 count += sizeof(ushort);
 foreach ({0} {1} in {1}List)
