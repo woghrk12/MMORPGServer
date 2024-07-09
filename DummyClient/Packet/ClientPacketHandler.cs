976bdd8c3488d6593508099353537e958fe1bbd5
황재호 <>
2024-07-09 21:52:49 +0900

    Insert the packet data

---
+++ DummyClient/Packet/ClientPacketHandler.cs
@@ -0,0 +1,15 @@
+using System;
+using ServerCore;
+
+namespace DummyClient
+{
+    public class PacketHandler
+    {
+          
+        public static void HandleChat(PacketSession session, IPacket packet)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Server/Packet/ServerPacketHandler.cs
@@ -0,0 +1,15 @@
+using System;
+using ServerCore;
+
+namespace Server
+{
+    public class PacketHandler
+    {
+          
+        public static void HandleChat(PacketSession session, IPacket packet)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
